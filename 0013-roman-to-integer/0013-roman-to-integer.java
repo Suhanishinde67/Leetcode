cb371c8eaa4939fccaa1a9b9c4c8a9261cb131be
Suhanishinde67 <shindesuhani535@gmail.com>
2026-07-06 21:34:53 +0530

    Time: 4 ms (55.64%), Space: 46.6 MB (37.53%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -12,13 +12,28 @@ class Solution {
         // Now every single character just gets added up directly
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (c == 'M') total += 1000;
-            else if (c == 'D') total += 500;
-            else if (c == 'C') total += 100;
-            else if (c == 'L') total += 50;
-            else if (c == 'X') total += 10;
-            else if (c == 'V') total += 5;
-            else if (c == 'I') total += 1;
+            if (c == 'M') 
+            {
+                total += 1000;
+            }else if (c == 'D') 
+            {
+                total += 500;
+            }else if (c == 'C') 
+            {
+                total += 100;
+            }else if (c == 'L') 
+            {
+                total += 50;
+            }else if (c == 'X') 
+            {
+                total += 10;
+            }else if (c == 'V') 
+            {
+                total += 5;
+            }else if (c == 'I') 
+            {
+                total += 1;
+            }
         }
         
         return total;
